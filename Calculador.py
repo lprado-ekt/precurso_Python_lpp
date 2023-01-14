436a5b1f4e2543774db75c67f580a2ae9644b876
LEOPOLDO PRADO PEREZ <945028@horusur.corp>
2023-01-14 10:01:35 -0600

    Primer commit

---
+++ Calculador.py
@@ -0,0 +1,37 @@
+
+    print("""
+    *** Selecciona la operación que deseas realizar y proporciona 2 números ***
+    1) Suma 
+    2) Resta
+    3) Multiplición
+    4) Divición
+    5) Raiz n
+    6) Exponente 
+    7) Seno 
+    8) Coseno 
+    9) Tangente 
+    10) Apagar calculadora
+    """)
+    if opciones == 1:
+        print("SUMA")
+    elif opciones == 2:
+        print("RESTA")
+    elif opciones == 3:
+        print("MULTIPLIACIÓN")
+    elif opciones == 4:
+        print("MULTIPLIACIÓN")
+    elif opciones == 5:
+        print("MULTIPLIACIÓN")
+    elif opciones == 6:
+        print("MULTIPLIACIÓN")
+    elif opciones == 7:
+        print("MULTIPLIACIÓN")
+    elif opciones == 8:
+        print("***OPERACIÓN NO DISPONIBLE POR EL MOMENTO**")
+    elif opciones == 9:
+        print("***OPERACIÓN NO DISPONIBLE POR EL MOMENTO**")
+    elif opciones == 10:
+        print("Hasta luego")
+        break
+    else:
+        print("Opción incorrecta")
